683a22c964de4e2ff39066d96bf1f63e81efc861
EC2 Default User <ec2-user@ip-172-31-16-88.eu-west-1.compute.internal>
2016-03-07 07:38:46 +0000

    updated groovy file

---
+++ jenkins/jobs/dsl/erpjobs.groovy
@@ -0,0 +1,38 @@
+// Folders
+def workspaceFolderName = "${WORKSPACE_NAME}"
+def projectFolderName = "${PROJECT_NAME}"
+
+// Variables
+def erpDEVUrl = "http://john.smith@52.48.3.198/gerrit/deploy"
+
+// Jobs
+def deploy_to_dev = freeStyleJob(projectFolderName + "/01_Deploy_to_DEV")
+def deplot_to_test = freeStyleJob(projectFolderName + "/02_Deplo_to_TEST")
+
+deploy_to_dev.with{
+  description()
+  scm{
+    git{
+      remote{
+        url(erpDEVUrl)
+        credentials("adop-jenkins-master")
+      }
+      branch("*/master")
+    }
+  }
+  steps {
+	batchFile ('cd C:\Oracle\Middleware\Oracle_SOA1\common\binwlst.cmd C:\AppDeployer.py -u weblogic -p welcome1 -a t3://localhost:7003 -d "C:\Program Files (x86)\Jenkins\jobs\Deploy_to_DEV\workspace" -t http://localhost:8003')
+ }	
+}
+
+deploy_to_test.with{
+  description()
+  steps {
+        batchFile ('cd C:\Oracle\Middleware\Oracle_SOA1\common\binwlst.cmd C:\AppDeployer.py -u weblogic -p welcome1 -a t3://localhost:7002 -d "C:\Program Files (x86)\Jenkins\jobs\Deploy_to_DEV\workspace" -t http://localhost:8002')
+	}
+}
+
+
+
+
+
